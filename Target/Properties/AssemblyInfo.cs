86dcc656a0be0a6481b89a85d55ca35870e631ef
Hanappi, Oliver <oliver.hanappi@rubicon.eu>
2018-08-23 13:18:07 +0200

    Fixed typo in version.

--- Target/Properties/AssemblyInfo.cs
+++ Target/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.2")]
 [assembly: AssemblyFileVersion("1.0.1.2")]
-[assembly: AssemblyInformationalVersion("1.0.1-patch-002")]
+[assembly: AssemblyInformationalVersion("1.0.1-patch002")]
